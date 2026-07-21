dfb4a60375f508b2df7891bdb4986021c5d42a5b
Gandi varshini <varshinigandi17@gmail.com>
2026-07-21 16:52:40 +0530

    Add files via upload

---
+++ DeepSkilling/Module-05-EF-Core/Lab 1_Undersatndig ORM/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetailInventory.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; } = new();
+    }
+}
---
+++ DeepSkilling/Module-05-EF-Core/Lab 1_Undersatndig ORM/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetailInventory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
