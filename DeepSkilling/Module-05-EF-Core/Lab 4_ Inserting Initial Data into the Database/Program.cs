4275eb9936ec0fa5f4e0b9eeda381ed3a8bcc2d9
Gandi varshini <varshinigandi17@gmail.com>
2026-07-21 16:59:47 +0530

    Add files via upload

---
+++ DeepSkilling/Module-05-EF-Core/Lab 4_ Inserting Initial Data into the Database/Program.cs
@@ -0,0 +1,41 @@
+﻿using RetailInventory.Data;
+using RetailInventory.Models;
+
+using var context = new AppDbContext();
+
+// Create Categories
+var electronics = new Category
+{
+    Name = "Electronics"
+};
+
+var groceries = new Category
+{
+    Name = "Groceries"
+};
+
+// Add Categories
+context.Categories.AddRange(electronics, groceries);
+
+// Create Products
+var product1 = new Product
+{
+    Name = "Laptop",
+    Price = 75000,
+    Category = electronics
+};
+
+var product2 = new Product
+{
+    Name = "Rice Bag",
+    Price = 1200,
+    Category = groceries
+};
+
+// Add Products
+context.Products.AddRange(product1, product2);
+
+// Save to Database
+context.SaveChanges();
+
+Console.WriteLine("Data inserted successfully!");
