13e8bcd603af645524905327cf7e89e06d8cb6e4
Gandi varshini <varshinigandi17@gmail.com>
2026-07-21 16:57:41 +0530

    Add files via upload

---
+++ DeepSkilling/Module-05-EF-Core/Lab 2_Setting Up the Database Context for a retail store/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+namespace RetailInventory.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(
+                "Server=localhost;Database=RetailInventoryDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
